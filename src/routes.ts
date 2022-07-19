9a77fb447b4a083c0963878388ebd1f1d5ab7b21
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-07-19 01:17:16 -0300

    feat(routes): rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { AuthProfessionalUserController } from "./modules/account/useCases/authProfessionalUser/AuthProfessionalUserController";
+import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
 import { CreateProfessionalUseController } from "./modules/professional/useCases/createProfessionalUser/CreateProfessionalUserController";
 
 const routes = Router();
 
 const createProfessionalUser = new CreateProfessionalUseController();
+const authProfessionalUser = new AuthProfessionalUserController();
+
+const createClient = new CreateClientController();
 
 routes.post("/professional/create", createProfessionalUser.handle);
+routes.post("/professional/auth", authProfessionalUser.handle);
+
+routes.post("/client/create", isAuthenticated, createClient.handle);
 
 export { routes };
