5f1d96e9a1004b70f222633551e89de26a038c24
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-07-20 01:28:06 -0300

    feat(errorHandling): cria tratativa para erros conhecidos do prisma

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import express, { NextFunction, Request, Response } from "express";
 
+import "express-async-errors";
 import { AppError } from "./errors/AppError";
 import { routes } from "./routes";
 
@@ -14,6 +16,15 @@ app.use(
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
         message: err.message,
+      });
+    }
+
+    if (err instanceof PrismaClientKnownRequestError) {
+      const errorMessage = err.meta.target[0];
+
+      return response.status(400).json({
+        code: err.code,
+        message: `O campo ${errorMessage} já existe.`,
       });
     }
 
