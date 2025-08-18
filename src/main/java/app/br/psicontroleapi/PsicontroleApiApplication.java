793c7418bc8278f6e8600c9130f2cd0ae4a9c9ad
Gabriel Gomes <gabrielgoomes.desouza@gmail.com>
2025-08-17 22:58:15 -0300

    initial commit

---
+++ src/main/java/app/br/psicontroleapi/PsicontroleApiApplication.java
@@ -0,0 +1,13 @@
+package app.br.psicontroleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicontroleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicontroleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/br/psicontroleapi/PsicontroleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.br.psicontroleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicontroleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
